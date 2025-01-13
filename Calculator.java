d9495247a32f0367e6164dfbe5cc5b812d4a3abf
James <faislabdu288@gmail.com>
2025-01-13 18:22:58 +0300

    handling divided by zero

--- Calculator.java
+++ Calculator.java
@@ -13,6 +13,8 @@ public class Calculator {
     }
 
     public int div(int a, int b) {
+        if (b == 0)
+            System.out.println("Error: Division by zero");
         return a / b;
     }
 }
